8881327d86f27f5e884bb3153916de3854a8840c
shamshad <shamshad@users.noreply.github.com>
2023-05-27 06:53:29 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Fremont',
+    'salary': '$10,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Newark',
+    'salary': '$20,000'
+  }
+]
+
+@app.route('/')
+def hello_world():
+  return render_template('home.html', jobs=JOBS)
+
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
+
